03ffd6e236d57fa3b8663333a26e204d1d44df54
marcoozzzz05 <andronico.marco05@gmail.com>
2024-12-12 12:09:32 +0100

    add if,else,else if

--- index.js
+++ index.js
@@ -36,12 +36,25 @@ while (n >= 1) {
     n--;
 } 
 
-let word = prompt('Inserisci una parola');
-console.log(word.length);
+//let word = prompt('Inserisci una parola');
+//console.log(word.length);
 
 let number1 = 7;
 if (number1 > 5) {
     console.log("La variabile è maggiore di 5")
 } else {
     console.log("La variabile è minore di 5")
+}
+
+let score = parseInt("48")
+if (score <= 100 && score >= 90) {
+    console.log("Voto ottimo");
+} else if (score <= 89 && score >= 70) {
+    console.log("Voto buono");
+} else if (score <= 69 && score >= 60) {
+    console.log("Voto sufficiente");
+} else if (score <= 59) {
+    console.log("Voto insufficiente");
+} else {
+    console.log("Voto non valido")
 }
