0fd3d9e89e525fa5e420dcdfd9ed7d608ec41ccc
marcoozzzz05 <andronico.marco05@gmail.com>
2024-12-20 12:30:53 +0100

    add contatoreChiamate

--- index.js
+++ index.js
@@ -209,12 +209,14 @@ console.log(words)
 
 
 class Automobile {
+    #contatoreChiamate = 0
     constructor(marca,modello,anno,chilometraggio) {
         this.marca = marca
         this.modello = modello
         this.anno = anno
         this.chilometraggio = chilometraggio
-    }
+
+    }        
     descrizione() {
     return `Quest'auto è un ${this.marca} ${this.modello} del ${this.anno}`;
     }
