fe9e01054c313bbf4a961af4c5d047410ac744bc
marcoozzzz05 <andronico.marco05@gmail.com>
2025-01-22 11:33:21 +0100

    add Cors

--- index.js
+++ index.js
@@ -1010,3 +1010,25 @@ async function esegui() {
         console.log("Errore nel caricamento", errore);
     }
 }
+
+
+const apiURL = 'https://api.example.com/data';
+
+fetch(apiURL, {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log('Dati ricevuti:', data);
+  })
+  .catch((error) => {
+    console.error('Errore:', error);
+  });
